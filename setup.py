4b4ec5461336d9cfd9ad0c40b184b6a2bf78bbe5
Stefano Fiorini <stefano.fiorini@slalom.com>
2023-02-27 14:49:45 -0600

    Bump version: 0.1.2 → 0.1.3

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='python-k8-sample',
-    version='0.1.2',
+    version='0.1.3',
     description='sample template for deploying a Python application to Kubernetes',
     author='Stefano Fiorini',
     author_email='stefano@example.com',
